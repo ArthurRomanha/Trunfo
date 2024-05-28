1a3bdce23a361ea6759e41d5e6e37b7a51e194ac
ArthurRomanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-05-28 17:10:53 -0300

    Lógica Básica Implantada

--- script.js
+++ script.js
@@ -1,33 +1,75 @@
-let cartas = new Map();
-cartas.set("gustavo", {id:"gustavo", forca:5, defesa:10, magia:5});//inicializa as cartas
-cartas.set("juan", {id:"juan", forca:10, defesa:5, magia:5});
-cartas.set("arthur", {id:"arthur", forca:5, defesa:5, magia:10});
+let imgMinhaCarta = document.getElementById('minhaCarta');
+let divCartaOponente = document.getElementById('cartaOponente');
 
-let nomesDasCartas = Array.from(cartas.keys()); 
-//transforma o nome das cartas em um array, para tratar melhor, devido à uma particularidade do map
-
-
-let numeroDaCartaDoAversario = Math.floor(Math.random()*nomesDasCartas.length);//numero aleatorio para a carta adversaria
-let cartaAversarioDaVez = nomesDasCartas[numeroDaCartaDoAversario];//escolhe qual será a carta a partir do seu nome
-let cartaAversario = cartas.get(cartaAversarioDaVez);//puxa o objeto CARTA a partir do nome selecionado
-console.log(cartaAversario);
-
-let forcaCartaAversario = cartaAversario.forca;
-let defesaCartaAversario = cartaAversario.defesa;
-let magiaCartaAversario = cartaAversario.magia;
-
-let numeroDaMinhaCarta = Math.floor(Math.random()*nomesDasCartas.length);
-
-while(numeroDaCartaDoAversario == numeroDaMinhaCarta){
-    //executa a funcao até que o numero aleatório da minha carta seja diferente da carta do adversário
-    numeroDaMinhaCarta = Math.floor(Math.random()*nomesDasCartas.length);
+let gustavo = {
+    nome: "Gustavo",
+    img: "",
+    atributos: {
+        forca: 5,
+        defesa: 10,
+        magia: 5
+    }
+}
+let juan = {
+    nome: "juan",
+    img: "",
+    atributos: {
+        forca: 10,
+        defesa: 5,
+        magia: 5
+    }
+}
+let arthur = {
+    nome: "arthur",
+    img: "",
+    atributos: {
+        forca: 5,
+        defesa: 5,
+        magia: 10
+    }
 }
 
-let minhaCartaDaVez = nomesDasCartas[numeroDaMinhaCarta]
-let minhaCarta = cartas.get(minhaCartaDaVez);
-console.log(minhaCarta);
+let cartas = [gustavo, juan, arthur];
+    let numeroDaMinhaCarta = Math.floor(Math.random() * cartas.length);
+    var minhaCarta = cartas[numeroDaMinhaCarta];
+    console.log(minhaCarta);
 
-let forcaminhaCarta = minhaCarta.forca;
-let defesaminhaCarta = minhaCarta.defesa;
-let magiaminhaCarta = minhaCarta.magia;
+    let numeroDaCartaDoAversario = Math.floor(Math.random() * cartas.length);//numero aleatorio para a carta do oponente
+    while (numeroDaCartaDoAversario == numeroDaMinhaCarta) {
+        //executa a funcao até que o numero aleatório da minha carta seja diferente da carta do oponente
+        numeroDaCartaDoAversario = Math.floor(Math.random() * cartas.length);
+    }
+    var cartaOponente = cartas[numeroDaCartaDoAversario];//escolhe qual será a carta a partir do seu nome
+    console.log(cartaOponente);
 
+    exibeAtributos();
+
+function exibeAtributos() {
+    let textoNomeMinhaCarta = document.getElementById('nomeMinhaCarta');
+    let textoForcaMinhaCarta = document.getElementById('forcaMinhaCarta');
+    let textoDefesaMinhaCarta = document.getElementById('defesaMinhaCarta');
+    let textoMagiaMinhaCarta = document.getElementById('magiaMinhaCarta');
+
+    textoNomeMinhaCarta.textContent = minhaCarta.nome;
+    textoForcaMinhaCarta.textContent = `Força: ${minhaCarta.atributos.forca}`
+    textoDefesaMinhaCarta.textContent = `Defesa: ${minhaCarta.atributos.defesa}`
+    textoMagiaMinhaCarta.textContent = `Magia: ${minhaCarta.atributos.magia}`
+}
+function comparaAtributos() {
+    var selectedAttribute = document.querySelector('input[name="atributo"]:checked');
+    console.log(selectedAttribute);
+    if (selectedAttribute) {
+        let valueSelectedAttribute = selectedAttribute.value;
+        console.log(valueSelectedAttribute);
+        if (minhaCarta.atributos[valueSelectedAttribute] > cartaOponente.atributos[valueSelectedAttribute]) {
+            let resultado = document.getElementById('resultado');
+            resultado.textContent = 'Você Venceu!'
+        } else if(minhaCarta.atributos[valueSelectedAttribute] == cartaOponente.atributos[valueSelectedAttribute]) {
+            resultado.textContent = 'Empatou!'
+        }else{
+            resultado.textContent = 'Perdeu!'
+        }
+    }else{
+        alert("Escolha um atributo para continuar!")
+    }
+}
