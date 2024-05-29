37189e88f69dc259baaf5a2e45d743cc232f599e
ArthurRomanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-05-29 09:02:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let imgMinhaCarta = document.getElementById('minhaCarta');
-let divCartaOponente = document.getElementById('cartaOponente');
-
 let gustavo = {
     nome: "Gustavo",
     img: "",
@@ -11,7 +8,7 @@ let gustavo = {
     }
 }
 let juan = {
-    nome: "juan",
+    nome: "Juan",
     img: "",
     atributos: {
         forca: 10,
@@ -20,7 +17,7 @@ let juan = {
     }
 }
 let arthur = {
-    nome: "arthur",
+    nome: "Arthur",
     img: "",
     atributos: {
         forca: 5,
@@ -31,7 +28,7 @@ let arthur = {
 
 let cartas = [gustavo, juan, arthur];
     let numeroDaMinhaCarta = Math.floor(Math.random() * cartas.length);
-    var minhaCarta = cartas[numeroDaMinhaCarta];
+    let minhaCarta = cartas[numeroDaMinhaCarta];
     console.log(minhaCarta);
 
     let numeroDaCartaDoAversario = Math.floor(Math.random() * cartas.length);//numero aleatorio para a carta do oponente
@@ -39,7 +36,7 @@ let cartas = [gustavo, juan, arthur];
         //executa a funcao até que o numero aleatório da minha carta seja diferente da carta do oponente
         numeroDaCartaDoAversario = Math.floor(Math.random() * cartas.length);
     }
-    var cartaOponente = cartas[numeroDaCartaDoAversario];//escolhe qual será a carta a partir do seu nome
+    let cartaOponente = cartas[numeroDaCartaDoAversario];//escolhe qual será a carta a partir do seu nome
     console.log(cartaOponente);
 
     exibeAtributos();
@@ -51,9 +48,9 @@ function exibeAtributos() {
     let textoMagiaMinhaCarta = document.getElementById('magiaMinhaCarta');
 
     textoNomeMinhaCarta.textContent = minhaCarta.nome;
-    textoForcaMinhaCarta.textContent = `Força: ${minhaCarta.atributos.forca}`
-    textoDefesaMinhaCarta.textContent = `Defesa: ${minhaCarta.atributos.defesa}`
-    textoMagiaMinhaCarta.textContent = `Magia: ${minhaCarta.atributos.magia}`
+    textoForcaMinhaCarta.textContent = `Força: ${minhaCarta.atributos.forca}`;
+    textoDefesaMinhaCarta.textContent = `Defesa: ${minhaCarta.atributos.defesa}`;
+    textoMagiaMinhaCarta.textContent = `Magia: ${minhaCarta.atributos.magia}`;
 }
 function comparaAtributos() {
     var selectedAttribute = document.querySelector('input[name="atributo"]:checked');
@@ -62,14 +59,28 @@ function comparaAtributos() {
         let valueSelectedAttribute = selectedAttribute.value;
         console.log(valueSelectedAttribute);
         if (minhaCarta.atributos[valueSelectedAttribute] > cartaOponente.atributos[valueSelectedAttribute]) {
+            mostraAdversario();
             let resultado = document.getElementById('resultado');
             resultado.textContent = 'Você Venceu!'
         } else if(minhaCarta.atributos[valueSelectedAttribute] == cartaOponente.atributos[valueSelectedAttribute]) {
-            resultado.textContent = 'Empatou!'
+            mostraAdversario();
+            resultado.textContent = 'Empatou!';
         }else{
-            resultado.textContent = 'Perdeu!'
+            mostraAdversario();
+            resultado.textContent = 'Perdeu!';
         }
     }else{
-        alert("Escolha um atributo para continuar!")
+        alert("Escolha um atributo para continuar!");
     }
+}
+function mostraAdversario(){
+    let textoNomeCartaOponente = document.getElementById('nomeCartaOponente');
+    let textoForcaCartaOponente = document.getElementById('forcaCartaOponente');
+    let textoDefesaCartaOponente = document.getElementById('defesaCartaOponente');
+    let textoMagiaCartaOponente = document.getElementById('magiaCartaOponente');
+
+    textoNomeCartaOponente.textContent = cartaOponente.nome;
+    textoForcaCartaOponente.textContent = `Força: ${cartaOponente.atributos.forca}`
+    textoDefesaCartaOponente.textContent = `Defesa: ${cartaOponente.atributos.defesa}`
+    textoMagiaCartaOponente.textContent = `Magia: ${cartaOponente.atributos.magia}`
 }
