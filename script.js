078a9434bcd7c36e46762e327d49249c740ca2b6
ArthurRomanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-07-02 09:39:17 -0300

    adicção da agilidade

--- script.js
+++ script.js
@@ -2,27 +2,30 @@ let gustavo = {
     nome: "Gustavo",
     img: "imagens/gustavo.jpg",
     atributos: {
-        forca: 5,
-        defesa: 10,
-        magia: 5
+        forca: 25,
+        defesa: 15,
+        magia: 40,
+        agilidade: 20
     }
 }
 let juan = {
     nome: "Juan",
-    img: "",
+    img: "imagens/juan.jpeg",
     atributos: {
-        forca: 10,
-        defesa: 5,
-        magia: 5
+        forca: 45,
+        defesa: 25,
+        magia: 20,
+        agilidade: 10
     }
 }
 let arthur = {
     nome: "Arthur",
     img: "imagens/arthur.jpg",
     atributos: {
-        forca: 5,
-        defesa: 5,
-        magia: 10
+        forca: 30,
+        defesa: 15,
+        magia: 35,
+        agilidade: 20
     }
 }
 
@@ -52,12 +55,14 @@ function exibeAtributos() {
     let textoForcaMinhaCarta = document.getElementById('forcaMinhaCarta');
     let textoDefesaMinhaCarta = document.getElementById('defesaMinhaCarta');
     let textoMagiaMinhaCarta = document.getElementById('magiaMinhaCarta');
+    let textoAgilidadeMinhaCarta = document.getElementById('agilidadeMinhaCarta');
 
     imgMinhaCarta.src = minhaCarta.img;
     textoNomeMinhaCarta.textContent = minhaCarta.nome;
     textoForcaMinhaCarta.textContent = `Força: ${minhaCarta.atributos.forca}`;
     textoDefesaMinhaCarta.textContent = `Defesa: ${minhaCarta.atributos.defesa}`;
     textoMagiaMinhaCarta.textContent = `Magia: ${minhaCarta.atributos.magia}`;
+    textoAgilidadeMinhaCarta.textContent = `Agilidade: ${minhaCarta.atributos.agilidade}`;
 }
 function comparaAtributos() {
     btnRecomecar.style.display = 'inline';
@@ -88,12 +93,14 @@ function mostraAdversario(){
     let textoForcaCartaOponente = document.getElementById('forcaCartaOponente');
     let textoDefesaCartaOponente = document.getElementById('defesaCartaOponente');
     let textoMagiaCartaOponente = document.getElementById('magiaCartaOponente');
+    let textoAgilidadeCartaOponente = document.getElementById('agilidadeCartaOponente');
 
     imgCartaOponente.src = cartaOponente.img;
     textoNomeCartaOponente.textContent = cartaOponente.nome;
     textoForcaCartaOponente.textContent = `Força: ${cartaOponente.atributos.forca}`
     textoDefesaCartaOponente.textContent = `Defesa: ${cartaOponente.atributos.defesa}`
     textoMagiaCartaOponente.textContent = `Magia: ${cartaOponente.atributos.magia}`
+    textoAgilidadeCartaOponente.textContent = `Agilidade: ${cartaOponente.atributos.agilidade}`
 }
 function recomecar(){
     window.location.reload();
